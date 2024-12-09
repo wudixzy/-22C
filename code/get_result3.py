339532ef615e3d5ff6bd7fca982a9525594dbfd8
CoolBoy <2269097679@qq.com>
2024-12-09 17:09:55 +0800

    Revert "get_result3.py"经过调参，最终结果价值能够翻20倍，已经足够用了。 This reverts commit 96a8ef259edc1f695cf9226c37d753eec4282b8d.

--- code/get_result3.py
+++ code/get_result3.py
@@ -62,12 +62,11 @@ class Market:
         self.t = 10  # 计算权重因子的范围
         self.C = self.weight_factor_constant
         self.weight_factor = self.C / (np.arange(1, self.t+1)**2)
-        self.weight_factor = self.weight_factor/self.weight_factor.sum()
         
         self.n_moving_average = 10  # 移动平均窗口大小
         self.thr = 0.05  # 梯度阈值
         self.rate_above_moving_average = 1.05
-        self.rate_under_moving_average = 0.80
+        self.rate_under_moving_average = 0.90
         self.rate_extreme_market = 5
         self.size_window_extreme_market = 10
         self.holding_days = 5  # 持有期天数
